6fd2d967ba8313a47b1b108b057591600ce476ea
Aditya Singh <42885369+adityasngh342@users.noreply.github.com>
2021-12-16 03:02:24 +0530

    Add files via upload

---
+++ src/BodyComponent/body.js
@@ -0,0 +1,97 @@
+import React from "react";
+import "./body.css";
+import Header from "./header";
+import SongRow from "./SongRow";
+import PlayCircleFilledIcon from "@material-ui/icons/PlayCircleFilled";
+import FavoriteIcon from "@material-ui/icons/Favorite";
+import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
+import {useDataLayerValue} from "../DataLayer"
+function Body({ spotify }) {
+  const [{ discover_weekly,userPlayList,token }, dispatch] = useDataLayerValue();
+  const playPlaylist = (id) => {
+    spotify
+      .play({
+        context_uri: `spotify:playlist:37i9dQZEVXcJZyENOWUFo7`,
+      })
+      .then((res) => {
+        spotify.getMyCurrentPlayingTrack().then((r) => {
+          dispatch({
+            type: "SET_ITEM",
+            item: r.item,
+          });
+          dispatch({
+            type: "SET_PLAYING",
+            playing: true,
+          });
+        });
+      });
+  };
+
+  const playSong = (id) => {
+    console.log(id)
+    console.log(token)
+    // const body = {
+    //   uris: [`spotify:track:${id}`],
+    // }    
+    const headers = {'Authorization':`Bearer ${token}`,'Content-Type': 'application/json',}
+
+    const body = JSON.stringify({ uris: [`spotify:track:${id}`] })
+
+    // return fetch('https://api.spotify.com/v1/me/player/play',{
+    //   method:'PUT',
+    //   body,
+    //   headers
+    // }).then((res)=>console.log(res))
+    //   .catch((err)=>console.log(err))
+
+    spotify
+      .play({
+        uris: [`spotify:track:${id}`],
+      })
+      .then((res) => {
+        spotify.getMyCurrentPlayingTrack().then((r) => {
+          console.log(r)
+          dispatch({
+            type: "SET_ITEM",
+            item: r.item,
+          });
+          dispatch({
+            type: "SET_PLAYING",
+            playing: true,
+          });
+        });
+      });
+  };
+
+  return (
+    <div className="body">
+      <Header spotify={spotify} />
+
+      <div className="body__info">
+        <img src={discover_weekly?.images[0].url} alt="" />
+        <div className="body__infoText">
+          <strong>PLAYLIST</strong>
+          <h2>Discover Weekly</h2>
+          <p>{discover_weekly?.description}</p>
+        </div>
+      </div>
+
+      <div className="body__songs">
+        <div className="body__icons">
+          <PlayCircleFilledIcon
+            className="body__shuffle"
+            onClick={playPlaylist}
+          />
+          <FavoriteIcon fontSize="large" />
+          <MoreHorizIcon />
+        </div>
+        
+        {discover_weekly?.tracks.items.map((item) => (
+          <SongRow playSong={playSong} track={item.track} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Body;
